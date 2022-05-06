2f7ed519a16f5256b93ecf01033540a368086012
Jonathan <jreberg98@gmail.com>
2022-05-06 11:40:52 +0200

    Prødv å få Jest til å virke

---
+++ testing/Home.test.js
@@ -0,0 +1,10 @@
+import { render } from "react-dom"
+import Home from "../screens/Home"
+
+describe("Loading home", () => {
+	describe("Rendering home", () => {
+		it("Has pressable", () => {
+
+		})
+	})
+})
